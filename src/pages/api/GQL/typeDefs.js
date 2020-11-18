e7c10dd435f125003f55f57bf2546013e566c5db
Sean Dillon <seandillon1224@gmail.com>
2020-11-17 22:34:24 -0500

    Added beginnings of typedefs to typeDefs.js

--- src/pages/api/GQL/typeDefs.js
+++ src/pages/api/GQL/typeDefs.js
@@ -1,21 +1,56 @@
 import { gql } from "apollo-server-micro";
 
 const typeDefs = gql`
-  type Thread {
-    _id: ID!
-    text: String!
+
+  type User {
+    id: ID!
+    joinedDate: Date!
+    name: String!
+    avatar: String!
+    email: String!
+    handle: String!
+    settings: Settings!
+    posts: [Posts]!
+    comments: [Comments]!
   }
 
-  input ThreadInput {
+  type Settings {
+    _id: ID!
+    user: ID!
+    ads: Boolean!
+    activated: Boolean!
+    dateUpdated: Date
+  }
+
+  type Posts {
+    _id: ID!
+    own: Boolean!
+    user: ID!
+    text: String!
+    comments: [Comments]!
+    date: Date!
+  }
+
+  type Comments {
+    _id: ID!
+    post: ID!
+    user: ID!
+    text: String!
+    comments: [Comments]!
+    date: Date!
+  }
+
+  input PostInput {
+    id: ID!
     text: String!
   }
 
   type Mutation {
-    addThread(input: ThreadInput): Thread
+    addPost(input: PostInput): Post
   }
 
   type Query {
-    allThreads: [Thread]
+    allPosts: [Post]
   }
 `;
 
